44755cae0ad427c315a3ba7aa39065b4d0cc2ee2
Harshit Bansal <69475476+HarshitBansal020@users.noreply.github.com>
2022-11-04 08:59:23 +0530

    Add files via upload

---
+++ String/StringBuilderEx.java
@@ -0,0 +1,68 @@
+package String;
+
+public class StringBuilderEx {
+	public static void main(String[] args) {
+        // TODO Auto-generated method stubG
+        
+        
+        //immutable concept of String
+          String s = "Nancy";
+          
+          
+          System.out.println(s+" gupta");
+          
+          s = s+ " Gupta";
+          String s1 = s+" Gupta"; System.out.println(s); System.out.println(s1);
+          
+          String s2 = " Gupta";
+         System.out.println(s2);
+       
+          
+        //String imp conversions    
+        
+        //char to String        
+        char ch = 'n';
+        
+        String chstr = Character.toString(ch);
+        System.out.println("char ch="+ch+" to String:"+chstr);
+        
+        //String to char
+        
+        String yesno = "n";
+        
+         ch =  yesno.charAt(0);
+         System.out.println("String "+yesno+" to char:"+ch);
+         System.out.println(ch);
+         
+         //char to unicode value
+         
+         ch = 'A';
+         Character x = ch;  //autoboxing char to Character
+         System.out.println(x);
+         
+         
+         int charval = Character.getNumericValue(ch);
+         
+         System.out.println("char ch="+ch+" to Unicode value:"+charval);
+         
+         
+        
+        //important methods of string
+        
+        String str = "Core Java Batch";
+        
+        String str1 = str.substring(5,8);
+        System.out.println(str1);
+        
+         str1 = str.substring(5);
+        System.out.println(str1);
+        
+        System.out.println(str.toLowerCase());
+        
+        //String to char array
+        char[] arr = str.toCharArray();
+        System.out.println(arr);
+        
+    }
+
+}
